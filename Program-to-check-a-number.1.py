4494c9d982bccbde1e1f22ab024997df1f8334e8
ompragash <om.apsara@gmail.com>
2017-08-26 13:31:47 +0530

    LEVEL: Decision making

---
+++ Program-to-check-a-number.1.py
@@ -0,0 +1,13 @@
+#Program to check a number using nested if
+
+num = float(input("Enter the number:"))
+
+if num >= 0:
+    if num == 0:
+        print("Zero")
+
+    else:
+        print("Positive number")
+
+else:
+    print("Negative number")
