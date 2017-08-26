8950de498282d69f0c52b3c4a65f8deb1e543362
ompragash <om.apsara@gmail.com>
2017-08-26 13:18:57 +0530

    LEVEL: Introduction

---
+++ Convert-celsius-to-farenheit.py
@@ -0,0 +1,7 @@
+#Python program to convert celsius to farenheit
+
+celsius = float(input("Enter the celsius: "))
+
+farenheit = (celsius * 1.8) + 32
+
+print("%0.1f degree Celsius is equal to %0.3f degree farenheit" %(celsius, farenheit))
