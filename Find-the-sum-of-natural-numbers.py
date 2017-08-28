a5f21aedc299bef4abbee3d3515a6d827c146fec
ompragash <om.apsara@gmail.com>
2017-08-28 14:05:41 +0530

    LEVEL: Decision Making

---
+++ Find-the-sum-of-natural-numbers.py
@@ -0,0 +1,13 @@
+#Python program to find the sum of natural numbers 
+
+num = int(input("Enter the number: "))
+
+if num < 0:
+    print("Enter a positive number.")
+
+else:
+    sum = 0
+    while(num > 0):
+        sum += num
+        num -= 1
+    print("The sum is", sum)
