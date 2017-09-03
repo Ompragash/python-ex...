c02e8d09d693a86a34630df9805dd1a98c703cbd
ompragash <om.apsara@gmail.com>
2017-09-03 22:59:33 +0530

    LEVEL: Function

---
+++ Convert_Decimal_to_binary_octal_hex.py
@@ -0,0 +1,9 @@
+#Program to convert decimal to binary, octal and hexadecimal
+
+deci = int(input("Enter the number:"))
+
+print("The decimal value of",deci,"is:")
+
+print(bin(deci),"in binary.")
+print(oct(deci),"in octal.")
+print(hex(deci),"in hexadecimal.")
